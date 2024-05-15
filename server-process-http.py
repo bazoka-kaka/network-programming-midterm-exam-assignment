aa2ede18d986ecea3a7cdaa722a68ff17b52453d
bazoka-kaka <yehezkielwiradhika@gmail.com>
2024-05-16 00:59:04 +0700

    update server-process-http

--- server-process-http.py
+++ server-process-http.py
@@ -1,74 +1,29 @@
-from socket import *
 import socket
-import time
-import sys
-import logging
-import multiprocessing
-from http import HttpServer
+from multiprocessing import Process
 
-httpserver = HttpServer()
+def handle_client(client_socket):
+    try:
+        request = client_socket.recv(1024)
+        print(f"Received: {request}")
+        response = b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!"
+        client_socket.sendall(response)
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        client_socket.close()
 
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("0.0.0.0", 8080))
+    server.listen(5)
+    print("Server listening on port 8080")
 
-class ProcessTheClient(multiprocessing.Process):
-	def __init__(self, connection, address):
-		self.connection = connection
-		self.address = address
-		multiprocessing.Process.__init__(self)
+    while True:
+        client_sock, addr = server.accept()
+        print(f"Accepted connection from {addr}")
+        process = Process(target=handle_client, args=(client_sock,))
+        process.start()
+        client_sock.close()
 
-	def run(self):
-		rcv=""
-		while True:
-			try:
-				data = self.connection.recv(32)
-				if data:
-					#merubah input dari socket (berupa bytes) ke dalam string
-					#agar bisa mendeteksi \r\n
-					d = data.decode()
-					rcv=rcv+d
-					if rcv[-2:]=='\r\n':
-						#end of command, proses string
-						#logging.warning("data dari client: {}" . format(rcv))
-						hasil = httpserver.proses(rcv)
-						#hasil akan berupa bytes
-						#untuk bisa ditambahi dengan string, maka string harus di encode
-						hasil=hasil+"\r\n\r\n".encode()
-						#logging.warning("balas ke  client: {}" . format(hasil))
-						#hasil sudah dalam bentuk bytes
-						self.connection.sendall(hasil)
-						rcv=""
-						self.connection.close()
-				else:
-					break
-			except OSError as e:
-				pass
-		self.connection.close()
-
-
-
-class Server(multiprocessing.Process):
-	def __init__(self):
-		self.the_clients = []
-		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		multiprocessing.Process.__init__(self)
-
-	def run(self):
-		self.my_socket.bind(('0.0.0.0', 8889))
-		self.my_socket.listen(1)
-		while True:
-			self.connection, self.client_address = self.my_socket.accept()
-			logging.warning("connection from {}".format(self.client_address))
-
-			clt = ProcessTheClient(self.connection, self.client_address)
-			clt.start()
-			self.the_clients.append(clt)
-
-
-
-def main():
-	svr = Server()
-	svr.start()
-
-if __name__=="__main__":
-	main()
-
+if __name__ == "__main__":
+    start_server()
